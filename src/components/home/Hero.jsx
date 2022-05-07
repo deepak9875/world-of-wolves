06f444bc21c17c318267a90b4a9bc28d40644451
HarshRadialCode <harshradialcode@gmail.com>
2022-05-07 12:55:45 +0530

    header and hero

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import Magic from "../../assets/img/magic.png";
+import HeroImg from "../../assets/img/heroImg.png";
 const Hero = () => {
   return (
-    <div className="flex-grow-1">
+    <div className="flex-grow-1 d-flex justify-content-center align-items-center">
       <div className="container">
         <div className="hero d-flex justify-content-between">
-          <div className="left-hero">
+          <div className="left-hero d-flex flex-column align-items-center justify-content-conter">
             <h1>
               World of Wolves is a unique concept including staking, breeding,
               customisation & evolution
             </h1>
-            <button className="px-4 py-2 bg-transparent btn-transparent">
+            <button className="px-4 my-2 py-2 bg-transparent btn-transparent">
               <img src={Magic} className="ms-2" alt="magic" />{" "}
               <span className="d-inline-block px-2">Magic Eden</span>
             </button>
+            <h2 className="mt-5 ms-3">Alpha Wolves  </h2>
+            <div className="d-flex mt-3 align-items-center">
+              <div className="d-flex title align-items-center flex-nowrap ms-3"> <span className="active">Supply:</span> <span className="detail ms-3">1,111</span></div>
+              <div className="d-flex title  align-items-center flex-nowrap ms-4"> <span className="active">Mint Date:</span> <span className="detail ms-3">3rd March 2022</span></div>
+              <div className="d-flex title align-items-center flex-nowrap ms-4"> <span className="active">Mint Price:</span> <span className="detail ms-3">0.8 SOL</span></div>
+              <div className="d-flex title align-items-center flex-nowrap ms-4"> <span className="active">Traits:</span> <span className="detail ms-3">169</span></div>
+
+            </div>
+
+
+            <h2 className="mt-5 ms-3">Luna Wolves </h2>
+            <div className="d-flex mt-3 align-items-center">
+              <div className="d-flex title align-items-center flex-nowrap ms-3"> <span className="active">Supply</span> <span className="detail ms-3">TBC</span></div>
+              <div className="d-flex title  align-items-center flex-nowrap ms-4"> <span className="active">Mint Date:</span> <span className="detail ms-3"> 17th May 2022</span></div>
+              <div className="d-flex title align-items-center flex-nowrap ms-4"> <span className="active">Mint Price:</span> <span className="detail ms-3"> TBC</span></div>
+              <div className="d-flex title align-items-center flex-nowrap ms-4"> <span className="active">Traits:</span> <span className="detail ms-3"> 170+</span></div>
+
+            </div>
           </div>
-          <div className="right-hero"></div>
+          <div className="right-hero">
+            <img src={HeroImg} className="w-100" alt="" />
+          </div>
         </div>
       </div>
     </div>
