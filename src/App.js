b758729a923c1de39be14a3d3657e05ac1d9c3d0
deepak9875 <deepaklakhlan1234@gmail.com>
2022-05-07 12:51:51 +0530

    overview section done now

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import "./App.css";
 import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import Hero from "./components/home/Hero";
+import Overview from "./components/home/Overview";
 
 function App() {
-  return (
-    <div className="body-wrapper">
-      <div className="hero-header-wrapper d-flex flex-column">
-        <Header />
-        <Hero />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="body-wrapper">
+            <div className="hero-header-wrapper d-flex flex-column">
+                <Header />
+                <Hero />
+                <Overview />
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
